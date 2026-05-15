5044539f06be10c15b206548ea95e7d2cde4f8dc
emdawg239 <emiliejosephson@yahoo.com>
2026-05-14 22:14:11 -0500

    Update 1_Behavioral_Findings.py

--- pages/1_Behavioral_Findings.py
+++ pages/1_Behavioral_Findings.py
@@ -3,6 +3,10 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+from auth import check_password
+
+check_password()
+
 
 # ------------------------------------------------
 # LOVE ISLAND DESIGN SYSTEM
